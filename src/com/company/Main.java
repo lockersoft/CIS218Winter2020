46fec67bf12ce40a540eab4d9f9bd62c7044c4bc
dave.jones <dave@lockersoft.com>
2020-01-15 08:51:35 -0800

    Added Dave class and some output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,21 @@ import static com.company.GetData.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        System.out.print("Hello World\n");
-        System.out.println("This is on the same line");
+  public static void main(String[] args) {
+    // write your code here
+    System.out.print("Hello World\n");
+    System.out.println("This is on the same line");
 
-        String user_input = getString("Please enter a number: ");
-        Integer num = getInt("Please enter a number: ");
-        try {
-            num = Integer.parseInt(user_input);
-        } catch ( NumberFormatException nfe ) {
-            num = 0;
-        }
-        System.out.print("You entered :");
-        System.out.println(num);
-        System.out.println();
+    String user_input = getString("Please enter a number: ");
+    Integer num = getInt("Please enter a number: ");
+    try {
+      num = Integer.parseInt(user_input);
+    } catch ( NumberFormatException nfe ) {
+      num = 0;
     }
+    System.out.print("You entered :");
+    System.out.println(num);
+    System.out.println();
+  }
 
 }
